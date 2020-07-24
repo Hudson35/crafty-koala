0f920824c05237eeeb2f0265717ec15eeacd4299
Samad Ahmed <samadahmed2019@gmail.com>
2020-07-23 22:46:39 -0500

    Init commit

---
+++ src/main/java/com/doc/QuickScan/QuickScanApplication.java
@@ -0,0 +1,13 @@
+package com.doc.QuickScan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickScanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickScanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/doc/QuickScan/QuickScanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doc.QuickScan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickScanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
